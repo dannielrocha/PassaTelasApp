1f7aa03c026dd7e604ea0834a1d927220aa32d1d
dannielrocha <dannielrn@hotmail.com>
2017-08-28 23:55:14 -0300

    Ajustes

---
+++ app/src/main/java/com/example/danniel/exerciciosapp/ListaActivity.java
@@ -0,0 +1,13 @@
+package com.example.danniel.exerciciosapp;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class ListaActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+}
--- app/src/main/java/com/example/danniel/exerciciosapp/Main2Activity.java
+++ app/src/main/java/com/example/danniel/exerciciosapp/Main2Activity.java
@@ -3,6 +3,8 @@ package com.example.danniel.exerciciosapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.danniel.exerciciosapp.R;
+
 public class Main2Activity extends AppCompatActivity {
 
     @Override
